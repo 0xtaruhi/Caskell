6e3f0821c43855245ff0bee051a1234716e507f5
0xtaruhi <zhang_zhengyi@outlook.com>
2025-04-15 01:06:18 +0800

    improve lazystream

--- include/lazystream.hpp
+++ include/lazystream.hpp
@@ -25,8 +25,8 @@ struct HasMember_insert<T, std::void_t<decltype(std::declval<T &>().insert(
                                std::declval<typename T::iterator>(),
                                std::declval<typename T::value_type>()))>>
     : std::true_type {};
-} // namespace impl
 
+} // namespace impl
 template <typename T> class RangeGenerator;
 template <typename T> class ContainerGenerator;
 template <typename Gen, typename Func> class MapGenerator;
@@ -62,12 +62,40 @@ template <typename Gen> struct GeneratorTraits<TakeGenerator<Gen>> {
 template <typename Derived> class Generator {
 public:
   using T = typename impl::GeneratorTraits<Derived>::ValueType;
-  std::optional<T> next() { return static_cast<Derived *>(this)->nextImpl(); }
+  std::optional<T> next() const {
+    return static_cast<const Derived *>(this)->nextImpl();
+  }
+};
+
+template <typename T>
+class RangeGenerator : public Generator<RangeGenerator<T>> {
+  mutable T current_;
+
+public:
+  using ValueType = T;
+  explicit RangeGenerator(T start) : current_(start) {}
+  std::optional<T> nextImpl() const { return current_++; }
+};
+
+template <typename Container>
+class ContainerGenerator : public Generator<ContainerGenerator<Container>> {
+  using Iterator = typename Container::const_iterator;
+  mutable Iterator current_, end_;
+
+public:
+  using ValueType = typename Container::value_type;
+  explicit ContainerGenerator(const Container &container)
+      : current_(container.begin()), end_(container.end()) {}
+  std::optional<ValueType> nextImpl() const {
+    if (current_ == end_)
+      return std::nullopt;
+    return *current_++;
+  }
 };
 
 template <typename Gen, typename Func>
 class MapGenerator : public Generator<MapGenerator<Gen, Func>> {
-  Gen gen_;
+  mutable Gen gen_;
   Func func_;
 
   using T = typename Gen::ValueType;
@@ -75,44 +103,42 @@ class MapGenerator : public Generator<MapGenerator<Gen, Func>> {
 
 public:
   using ValueType = U;
-  std::optional<U> nextImpl() {
-    if (auto item = gen_.next()) {
-      return func_(*item);
-    }
-    return std::nullopt;
-  }
-
   MapGenerator(Gen gen, Func func)
       : gen_(std::move(gen)), func_(std::move(func)) {}
+  std::optional<U> nextImpl() const {
+    if (auto item = gen_.next())
+      return func_(*item);
+    return std::nullopt;
+  }
 };
 
 template <typename Gen, typename Pred>
 class FilterGenerator : public Generator<FilterGenerator<Gen, Pred>> {
-  Gen gen_;
+  mutable Gen gen_;
   Pred pred_;
 
 public:
   using ValueType = typename Gen::ValueType;
-  std::optional<ValueType> nextImpl() {
+  FilterGenerator(Gen gen, Pred pred)
+      : gen_(std::move(gen)), pred_(std::move(pred)) {}
+  std::optional<ValueType> nextImpl() const {
     while (auto item = gen_.next()) {
       if (pred_(*item))
         return item;
     }
     return std::nullopt;
   }
-
-  FilterGenerator(Gen gen, Pred pred)
-      : gen_(std::move(gen)), pred_(std::move(pred)) {}
 };
 
 template <typename Gen>
 class TakeGenerator : public Generator<TakeGenerator<Gen>> {
-  Gen gen_;
-  std::size_t remaining_;
+  mutable Gen gen_;
+  mutable std::size_t remaining_;
 
 public:
   using ValueType = typename Gen::ValueType;
-  std::optional<ValueType> nextImpl() {
+  TakeGenerator(Gen gen, std::size_t n) : gen_(std::move(gen)), remaining_(n) {}
+  std::optional<ValueType> nextImpl() const {
     if (remaining_ == 0)
       return std::nullopt;
     auto item = gen_.next();
@@ -122,77 +148,53 @@ public:
     }
     return std::nullopt;
   }
-
-  TakeGenerator(Gen gen, std::size_t n) : gen_(std::move(gen)), remaining_(n) {}
-};
-
-template <typename T>
-class RangeGenerator : public Generator<RangeGenerator<T>> {
-  T current_;
-
-public:
-  using ValueType = T;
-  explicit RangeGenerator(T start) : current_(start) {}
-
-  std::optional<T> nextImpl() { return current_++; }
-};
-
-template <typename Container>
-class ContainerGenerator : public Generator<ContainerGenerator<Container>> {
-  using Iterator = typename Container::const_iterator;
-  Iterator current_, end_;
-
-public:
-  using ValueType = typename Container::value_type;
-
-  ContainerGenerator(const Container &container)
-      : current_(container.begin()), end_(container.end()) {}
-
-  std::optional<ValueType> nextImpl() {
-    if (current_ == end_)
-      return std::nullopt;
-    return *current_++;
-  }
 };
 
 template <typename Gen> class LazyStream {
   Gen generator_;
 
 public:
-  explicit LazyStream(Gen gen) : generator_(std::move(gen)) {}
   using ValueType = typename Gen::ValueType;
 
-  template <typename Func> auto map(Func f) const {
-    using MappedGen = MapGenerator<Gen, Func>;
-    return LazyStream<MappedGen>(MappedGen{generator_, std::move(f)});
+  LazyStream(Gen &&gen) : generator_(std::forward<Gen>(gen)) {}
+
+  template <typename Func> auto map(Func &&f) const {
+    using F = std::decay_t<Func>;
+    using MappedGen = MapGenerator<Gen, F>;
+    return LazyStream<MappedGen>(MappedGen(generator_, std::forward<Func>(f)));
   }
 
-  template <typename Pred> auto filter(Pred p) const {
-    using FilteredGen = FilterGenerator<Gen, Pred>;
-    return LazyStream<FilteredGen>(FilteredGen{generator_, std::move(p)});
+  template <typename Pred> auto filter(Pred &&p) const {
+    using P = std::decay_t<Pred>;
+    using FilteredGen = FilterGenerator<Gen, P>;
+    return LazyStream<FilteredGen>(
+        FilteredGen(generator_, std::forward<Pred>(p)));
   }
 
   auto take(std::size_t n) const {
     using TakenGen = TakeGenerator<Gen>;
-    return LazyStream<TakenGen>(TakenGen{generator_, n});
+    return LazyStream<TakenGen>(TakenGen(generator_, n));
   }
 
-  template <typename U, typename Reducer> U reduce(U init, Reducer r) {
-    while (auto item = generator_.next()) {
+  template <typename U, typename Reducer> U reduce(U init, Reducer &&r) const {
+    auto copy = generator_;
+    while (auto item = copy.next()) {
       init = r(init, *item);
     }
     return init;
   }
 
-  template <typename Func> void forEach(Func f) {
-    while (auto item = generator_.next()) {
+  template <typename Func> void forEach(Func &&f) const {
+    auto copy = generator_;
+    while (auto item = copy.next()) {
       f(*item);
     }
   }
 
-  template <typename Container> Container collect() {
+  template <typename Container> Container collect() const {
+    auto copy = generator_;
     Container container;
-    while (auto item = generator_.next()) {
+    while (auto item = copy.next()) {
       if constexpr (impl::HasMember_push_back<Container>::value) {
         container.push_back(*item);
       } else if constexpr (impl::HasMember_insert<Container>::value) {
@@ -200,16 +202,14 @@ public:
       } else {
         static_assert(impl::HasMember_push_back<Container>::value ||
                           impl::HasMember_insert<Container>::value,
-                      "Container must have either push_back or insert method");
+                      "Container must have push_back or insert");
       }
     }
     return container;
   }
 
-  auto next() { return generator_.next(); }
-
   class Iterator {
-    Gen *generator_;
+    const Gen *generator_;
     std::optional<ValueType> current_;
 
   public:
@@ -218,7 +218,7 @@ public:
     using IteratorCategory = std::input_iterator_tag;
     using DifferenceType = std::ptrdiff_t;
 
-    explicit Iterator(Gen *gen)
+    explicit Iterator(const Gen *gen)
         : generator_(gen), current_(generator_->next()) {}
 
     Iterator() : generator_(nullptr), current_(std::nullopt) {}
@@ -234,12 +234,11 @@ public:
     bool operator==(const Iterator &other) const {
       return (!current_ && !other.current_);
     }
-
     bool operator!=(const Iterator &other) const { return !(*this == other); }
   };
 
-  Iterator begin() { return Iterator(&generator_); }
-  Iterator end() { return Iterator(); }
+  Iterator begin() const { return Iterator(&generator_); }
+  Iterator end() const { return Iterator(); }
 };
 
 } // namespace caskell
